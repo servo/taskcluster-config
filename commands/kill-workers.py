b363e61b68b51dab4aa9ee93e39667bed8a7ae1a
Simon Sapin <simon.sapin@exyr.org>
2019-11-03 22:35:19 +0100

    Handle paginated worker list

--- commands/kill-workers.py
+++ commands/kill-workers.py
@@ -4,12 +4,18 @@ import taskcluster
 
 def main(worker_pool):
     wm = taskcluster.WorkerManager(taskcluster.optionsFromEnvironment())
-    result = wm.listWorkersForWorkerPool("proj-servo/" + worker_pool)
-    workers = [w for w in result["workers"] if w["state"] != "stopped"]
-
-    for w in workers:
-        print(w["workerGroup"], w["workerId"], w["state"])
-    print()
+    workers = []
+    stopped = 0
+    def handle(result):
+        nonlocal stopped
+        for w in result["workers"]:
+            if w["state"] == "stopped":
+                stopped += 1
+            else:
+                print(w["workerGroup"], w["workerId"], w["state"])
+                workers.append(w)
+    wm.listWorkersForWorkerPool("proj-servo/" + worker_pool, paginationHandler=handle)
+    print("… and %s stopped" % stopped)
 
     if input("Remove all? y/n ") == "y":
         for w in workers:
