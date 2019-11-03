a7b14f312fccb583c1b71f9b6fd6bfb7a68caa0c
Simon Sapin <simon.sapin@exyr.org>
2019-11-03 23:37:25 +0100

    Salt: flatten the directory structure

---
+++ macos/pillar/external_data.py
@@ -0,0 +1,15 @@
+import urllib
+import taskcluster
+
+
+def ext_pillar(_minion_id, _pillar, *_args):
+    secrets = taskcluster.Secrets(taskcluster.optionsFromEnvironment())
+    data = secrets.get("project/servo/tc-client/worker/macos/1")["secret"]
+
+    url = "https://raw.githubusercontent.com/servo/saltfs/master/admin/files/ssh/%s.pub"
+    data["ssh_keys"] = [urllib.urlopen(url % name).read() for name in [
+        "jdm",
+        "manishearth",
+        "simonsapin",
+    ]]
+    return data
