6706d2f4a15d1f441d4201c126c85adcc145ac18
Simon Sapin <simon.sapin@exyr.org>
2019-11-14 20:22:11 +0100

    ./mach creds: detect missing scopes
    Work around https://bugzilla.mozilla.org/show_bug.cgi?id=1596529

--- commands/creds.py
+++ commands/creds.py
@@ -1,4 +1,5 @@
 import click
+import html
 import os
 import sys
 import taskcluster
@@ -35,12 +36,32 @@ def creds(port, name, description, expires, scopes):
     root_url = os.environ.get("TASKCLUSTER_ROOT_URL", DEFAULT_ROOT_URL)
     client_id = None
     access_token = None
+    missing_scopes = None
     def wsgi_app(environ, start_response):
         nonlocal client_id
         nonlocal access_token
+        nonlocal missing_scopes
         query = urllib.parse.parse_qs(environ['QUERY_STRING'])
         client_id, = query["clientId"]
         access_token, = query["accessToken"]
+
+        auth = taskcluster.Auth({
+            "rootUrl": root_url,
+            "credentials": {"clientId": client_id, "accessToken": access_token},
+        })
+        missing_scopes = set(scopes) - set(auth.currentScopes()["scopes"])
+        if missing_scopes:
+            start_response("401 Unauthorized", [("Content-Type", "text/html")])
+            return [b"""
+                <!doctype html>
+                <title>Missing scopes</title>
+                <h1>You have successfully signed in,
+                    but are missing some of the requested scopes</h1>
+                <ul>
+            """ + "\n".join(
+                ("<li><code>%s</code></li>" % html.escape(scope))
+                for scope in sorted(missing_scopes)
+            ).encode("utf-8")]
 
         start_response("200 OK", [("Content-Type", "text/html")])
         return [b"""
@@ -75,6 +96,12 @@ def creds(port, name, description, expires, scopes):
     sys.stderr.write("Exporting TASKCLUSTER_CLIENT_ID and TASKCLUSTER_ACCESS_TOKEN\n")
     print("export TASKCLUSTER_CLIENT_ID='%s'" % client_id)
     print("export TASKCLUSTER_ACCESS_TOKEN='%s'" % access_token)
+
+    if missing_scopes:
+        sys.stderr.write("The new client is missing scopes:\n")
+        for scope in sorted(missing_scopes):
+            sys.stderr.write("* %s\n" % scope)
+        sys.exit(1)
 
 
 if __name__ == "__main__":
